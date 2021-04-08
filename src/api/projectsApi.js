de9aa55238ed81cddaa9b6ec33e91a4e160188c5
abelkm99 <abelkidanemariam99@gmail.com>
2021-04-08 13:37:48 +0300

    can see users profile

--- src/api/projectsApi.js
+++ src/api/projectsApi.js
@@ -48,6 +48,9 @@ function createNewProject(data){
 }
 
 function getMembers(){
+    
+    var token = localStorage.getItem('access_token') || ''
+    token = token.substring(1,token.length-1)
     var config = {
     method: 'get',
     url: 'http://127.0.0.1:5000/api_v1/members',
