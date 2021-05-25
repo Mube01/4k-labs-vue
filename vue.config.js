3de9a1309cfa1cf8f2e873cb3e983b93c4b80df1
abelkm99 <abelkidanemariam99@gmail.com>
2021-05-25 16:02:54 +0300

    change the port tp 8080

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api_v1': {
-        target: 'http://127.0.0.1:5000',
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true
       },
     }
