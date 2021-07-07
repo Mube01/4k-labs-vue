c5e201b93747dc5f82f5d4594111976c7fbd4842
abelkm99 <abelkidanemariam99@gmail.com>
2021-07-07 19:35:42 +0300

    final fixes

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/api_v1': {
-        target: 'http://127.0.0.1:5000/',
-        // target: 'https://www.4kbots.com',
+        // target: 'http://127.0.0.1:5000/',
+        target: 'https://www.4kbots.com',
         changeOrigin: true
       },
     }
