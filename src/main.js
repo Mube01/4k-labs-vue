419b20ce072f48764495b99124ed1291e0c92930
teyouale <teyouale@gmail.com>
2021-11-12 12:49:59 +0300

    Installing Tailwind CSS

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "@/styles/app.css";
 
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+	.use(store)
+	.use(router)
+	.mount("#app");
